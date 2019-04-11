8f8419997b792141971889e09c75ac785dd1cb5a
lcy <549229632@qq.com>
2019-04-11 09:09:16 +0800

    打包

---
+++ build/precache-manifest.6863cd7769b5d9bec1fb71876b02999e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2427fb7ec75d7e0d3f65",
+    "url": "/static/js/main.2427fb7e.chunk.js"
+  },
+  {
+    "revision": "040e6f71d90800d90687",
+    "url": "/static/js/1.040e6f71.chunk.js"
+  },
+  {
+    "revision": "2427fb7ec75d7e0d3f65",
+    "url": "/static/css/main.b4404673.chunk.css"
+  },
+  {
+    "revision": "30c753bfbc3df263f0a8e7b126f814f6",
+    "url": "/index.html"
+  }
+];
